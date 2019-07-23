7d2c9ea76999cc3a5d7ef546b78322623ef10f8d
Diego Hamet <51054037+hamet322@users.noreply.github.com>
2019-07-23 17:03:56 -0500

    eliminando repositorio con error

--- src/main/java/pe/edu/unsch/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.unsch.dao;
-
-import pe.edu.unsch.entities.Product;
-
-public interface ProductDao {
-	
-	public Product find(Integer  id);
-
-}
--- src/main/java/pe/edu/unsch/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.unsch.service;
-
-import pe.edu.unsch.entities.Product;
-
-public interface ProductService {
-	
-	public Product find(Integer  id);
-
-}
